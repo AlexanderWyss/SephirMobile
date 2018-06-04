626db35bc7848c1fe34727a6a1cbf1583eddf978
Whalesharkking <elias_christen@gmx.ch>
2018-06-04 19:32:57 +0200

    Removed unused Code Start MarksActivity after LoginActivity

--- app/src/main/java/application/sephirmobile/LoginActivity.java
+++ app/src/main/java/application/sephirmobile/LoginActivity.java
@@ -3,6 +3,7 @@ package application.sephirmobile;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -19,6 +20,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -124,6 +126,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
+        startActivity(new Intent(this, MarksActivity.class));
 
         }
     }
