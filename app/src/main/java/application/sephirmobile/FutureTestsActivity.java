d139e38f9be0026f394b0580e64ef699009fbc95
Whalesharkking <elias_christen@gmx.ch>
2018-06-04 19:57:50 +0200

    Added FutureTestsActivity and linked it with Menu

--- app/src/main/java/application/sephirmobile/FutureTestsActivity.java
+++ app/src/main/java/application/sephirmobile/FutureTestsActivity.java
@@ -1,21 +1,17 @@
 package application.sephirmobile;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
-public class MarksActivity extends AppCompatActivity
+public class FutureTestsActivity  extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
@@ -62,4 +58,5 @@ public class MarksActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
